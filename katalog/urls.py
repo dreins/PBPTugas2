1e8f1f9bef9f388c5e039d4eee1f21aea924cbb4
dreins <davynreinhard@gmail.com>
2022-09-11 02:49:09 +0700

    Tugas 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import katalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', katalog, name='katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+data_katalog = CatalogItem.objects.all()
+
+def katalog(request):
+    context = {
+    'katalog_items': data_katalog,
+    'nama': 'Davyn Reinhard',
+    'student_id': '2106751083'
+    }
+    return render(request, 'katalog.html', context)
